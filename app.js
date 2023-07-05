f37c2421690508e855264818c26f884ca228c65d
garcia-f <garciafabianemmanuel@gmail.com>
2023-07-05 14:32:52 -0300

    Complete el código faltante en el archivo de arranque del servidor (app.js)

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 // Imports
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const express = require('express');
 
 const path = require('path');
 
 const app = express();
 
+const { sequelize } = require('./database');
+
+sequelize.authenticate()
+    .then(() => console.log('Conexión a base de datos exitosa'))
+    .catch((error) => console.log('Error al conectar a base de datos', error));
+
+require('ejs');
+app.set('view engine', 'ejs');
+
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+//app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`)
+});
+
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on port ${port}`));
