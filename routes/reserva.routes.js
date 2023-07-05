dd8687f6532fcfb1f4c407d50d46c6883a246104
garcia-f <garciafabianemmanuel@gmail.com>
2023-07-05 15:09:33 -0300

    rutas para el crud de reservas

--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -2,32 +2,52 @@
 
 const router = require('express').Router();
 
+const {
+    obtenerReservas,
+    obtenerReserva,
+    crearReserva,
+    actualizarReserva,
+    eliminarReserva
+} = require('../controllers/reserva.controllers')
+
 
 // ==========================================
 //         Rutas para renderizar vistas
 // ==========================================
 
 // Obtener todas las reservas
+router.get('/', (req, res) => {
+    res.render('index');
+})
 
 // Formulario para crear una reserva
+router.get('/reserva/crear', (req, res) => {
+    res.render('reserva/crear_reserva');
+})
 
 // Formulario para actualizar una reserva
+router.get('/reserva/editar_reserva', (req, res) => {
+    const reservaId = req.params.id;
+    res.render('reserva/editar_reserva', { id: reservaId })
+});
 
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-router.get('/api/',);
+router.get('/api/', obtenerReservas);
  
 // Crear una reserva
-router.post('/api/',);
+router.post('/api/', crearReserva);
+
+router.get('/api/:id', obtenerReserva);
  
 // Actualizar una reserva
-router.put('/api/:id',);
+router.put('/api/:id', actualizarReserva);
  
 // Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
+router.delete('/api/:id', eliminarReserva);
 
  
  module.exports = router;
